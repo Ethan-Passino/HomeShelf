dc6ce09a9275daa8a2b7250c133b29379224b088
Ethan Passino <itzethanpassino@gmail.com>
2025-06-18 21:07:45 -0400

    add .env

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -10,13 +10,13 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCJNAlr7gbvZw3wuQrzIHZOt2QSEdPhC74',
-  authDomain: 'homeshelf-b7469.firebaseapp.com',
-  projectId: 'homeshelf-b7469',
-  storageBucket: 'homeshelf-b7469.firebasestorage.app',
-  messagingSenderId: '22697074287',
-  appId: '1:22697074287:web:338408ba1f1a17031c379a',
-  measurementId: 'G-87X0WW2NLY',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
